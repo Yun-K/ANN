b67f7cde7f5cbfbea9f9ff1e4481bc1e0d0d4b30
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-05-02 22:01:13 +0800

    add scanner to scan the file

--- comp307_a2Yun/src/part2/JGAPTest.java
+++ comp307_a2Yun/src/part2/JGAPTest.java
@@ -1,10 +1,54 @@
 package part2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.jgap.InvalidConfigurationException;
 import org.jgap.gp.GPProblem;
 import org.jgap.gp.impl.GPGenotype;
 
 public class JGAPTest {
+
+    List<Double> X_LIST = new ArrayList<>();
+
+    List<Double> Y_LIST = new ArrayList<>();
+
+    private void readFile() {
+        String filePath = "regression.txt";
+        try {
+            Scanner scan = new Scanner(new File(filePath));
+            // first 2 lines are reduntatnt, so skip it
+            scan.nextLine();
+            scan.nextLine();
+
+            // do the iteration to read remianing lines
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                Scanner scanLine = new Scanner(line);
+                int index = -1;
+                while (scanLine.hasNextDouble()) {
+                    index++;
+                    if (index == 0) {
+                        X_LIST.add(scanLine.nextDouble());
+                    } else if (index == 1) {
+                        Y_LIST.add(scan.nextDouble());
+                    } else {
+                        // assertion check, should not be exxecuted
+                        assert false;
+                    }
+                }
+                scanLine.close();// close to save resources
+            }
+            scan.close();// close to save resources
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String args[]) throws InvalidConfigurationException {
         GPProblem problem = new GPProblem1();
         GPGenotype gp = problem.create();
