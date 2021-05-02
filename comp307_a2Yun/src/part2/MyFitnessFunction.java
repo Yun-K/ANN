d77ebe449d566258520f1c305419a65ba7b0da97
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-05-03 01:19:34 +0800

    by looking at the online tutorial, implement MyFitnessFunction::evaluate(IGProgram)

--- comp307_a2Yun/src/part2/MyFitnessFunction.java
+++ comp307_a2Yun/src/part2/MyFitnessFunction.java
@@ -11,7 +11,8 @@ import org.jgap.gp.terminal.Variable;
  * A fitness function which reflects the ability of an individual to survive and reproduce
  * ("survival of the fittest").
  * 
- * 
+ * <p>
+ * https://cvalcarcel.wordpress.com/2009/08/04/jgap-a-firstsimple-tutorial/
  * 
  * @author Yun Zhou 300442776
  * @version
@@ -25,8 +26,23 @@ public class MyFitnessFunction extends GPFitnessFunction {
 
     List<Double> x_LIST, y_LIST;
 
+    /**
+     * they are references to objects under the control of the chromosomes in the population
+     * and having them means we can give the chromosomes new values to help them do their job:
+     * figuring out what the formula is that will get us the desired output.
+     */
     Variable _xVariable, _yVariable;
 
+    /**
+     * A constructor. It construct a new instance of MyFitnessFunction.
+     *
+     * @param x_LIST
+     *            input
+     * @param y_LIST
+     *            output
+     * @param _xVariable
+     * @param _yVariable
+     */
     public MyFitnessFunction(
             List<Double> x_LIST, List<Double> y_LIST, Variable _xVariable,
             Variable _yVariable) {
@@ -38,10 +54,32 @@ public class MyFitnessFunction extends GPFitnessFunction {
 
     }
 
+    /**
+     * This function is for evaluating in order to choose the instance.
+     * 
+     * @see org.jgap.gp.GPFitnessFunction#evaluate(org.jgap.gp.IGPProgram)
+     */
     @Override
-    protected double evaluate(IGPProgram arg0) {
-        // TODO Auto-generated method stub
-        return 0;
+    protected double evaluate(IGPProgram program) {
+        double result = 0.0f;
+        long longResult = 0;
+        for (int i = 0; i < x_LIST.size(); i++) {
+            // Set the input values
+            _xVariable.set(x_LIST.get(i));
+            // _yVariable.set(_input2[i]);//in our assignment, y is not input
+
+            // Execute the genetically engineered algorithm
+            long value = program.execute_int(0, new Object[0]);// by looking at the source
+                                                               // code, obtain that NO_ARG is
+                                                               // new Object[0]
+
+            // The closer longResult gets to 0 the better the algorithm.
+            longResult += Math.abs(value - y_LIST.get(i));
+        }
+
+        result = longResult;
+
+        return result;
     }
 
 }
